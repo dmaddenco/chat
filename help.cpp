15a06c03f3be858454b2af8269d66dab087eb41b
David Madden <dmaddenco@gmail.com>
2017-09-07 17:12:10 -0600

    finished help message

--- help.cpp
+++ help.cpp
@@ -2,3 +2,17 @@
 // Created by David Madden on 9/7/17.
 //
 
+#include "help.h"
+
+void Help::error(){
+	cout << "Incorrect Number of Arguments" << endl;
+}
+
+void Help::message() {
+	cout << "****************************************HELP****************************************" << endl;
+	cout << "\tThis is a chat program, which includes a server and a client" << endl;
+	cout << "\tTo invoke the server: $./chat" << endl;
+	cout << "\tTo invoke the client: $./chat -p <port number> -s <IP address of friend>" << endl;
+	cout << "\tTo see this message again: $./chat -h" << endl;
+	cout << "************************************************************************************" << endl;
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+using std::cout;
+using std::endl;
+using std::cerr;
+#include <string>
+using std::string;
+
 #include "server.h"
 #include "client.h"
+#include "help.h"
 
-int main() {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+int main(int argc, char* argv[]) {
+	Help help;
+	if (argc == 1) {
+		//TODO Create server
+		cout << "Server" << endl;
+	} else if (argc == 5) {
+		//TODO Create client
+		cout << "Client" << endl;
+	} else if (argc == 2) {
+		string flag = argv[1];
+		if (flag.compare("-h") == 0){
+			help.message();
+		}
+	} else {
+		help.error();
+	}
 }
